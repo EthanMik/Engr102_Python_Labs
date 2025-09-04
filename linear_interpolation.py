fd677f8f71937131f303e9b4a7488c9bc3dc35ae
EthanMik <etmik@icloud.com>
2025-09-04 12:17:14 -0500

    Updated Interpolation

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -14,21 +14,38 @@
 # Date: 9/2/2025
 #
 
+import math
+
+# Calculate slope
 def slope(y2, y1, x2, x1):
     return (y2 - y1) / (x2 - x1)
 
-def calc(time):
-    if time == 300:
-        return 10222.078642554414
-    x1 = 10
-    x2 = 55
-    y1 = 2029
-    y2 = 23029
+# Interpolate between two vectors at a given time
+def interpolate(time, x1, x2, y1, y2):
     return slope(y2, y1, x2, x1) * (time - x1) + y1
 
-# Printing output
+# Orbit radius in kilometers
+orbit_radius = 6745
+# Circumference of the orbit
+circumference = orbit_radius * math.pi * 2
+
+# Given points
+x1 = 10
+x2 = 55
+y1 = 2029
+y2 = 23029
+
+# Times to interpolate
+t1 = 25
+t2 = 300
+
+# Calculate positions at t = 25 and t = 300
+position1 = interpolate(t1, x1, x2, y1, y2) % circumference
+position2 = interpolate(t2, x1, x2, y1, y2) % circumference
+
+# Print results
 print("Part 1:")
-print(f"For t = 25 minutes, the position p = {calc(25)} kilometers")
+print(f"For t = 25 minutes, the position p = {position1} kilometers")
 print("Part 2:")
-print(f"For t = 300 minutes, the position p = {calc(300)} kilometers")
+print(f"For t = 300 minutes, the position p = {position2} kilometers")
 
