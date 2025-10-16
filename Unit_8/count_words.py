708fa4e86ebccce9f6162fc17d8cd8f66837d8cf
EthanMik <etmik@icloud.com>
2025-10-16 15:34:16 -0500

    Unit 8 Challenge lab

---
+++ Unit_8/count_words.py
@@ -0,0 +1,19 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   *I have not given or received any unauthorized aid on this assignment."
+#
+# Name:       Ethan Mikolaycik
+# Section:    563
+# Assignment: Print Math 
+# Date:       8/27/2025
+
+WORDS = {}
+
+def remove_delimeters() -> str:
+    
+
+def print_word_amounts(sentence: str, words: str):
+    s = sentence.strip(".,?!")
+    print(s)
+
+print_word_amounts("Hullabaloo, Caneck! Caneck! Hullabaloo, Caneck! Caneck!", "Hullabaloo Caneck Whoop")
