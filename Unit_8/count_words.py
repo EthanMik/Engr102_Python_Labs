95b5df3a1ff8d2df1350f80a0db2b18017973a47
EthanMik <mikolaycikethan@gmail.com>
2025-10-16 18:54:55 -0500

    Unit 8 Challenge complete

--- Unit_8/count_words.py
+++ Unit_8/count_words.py
@@ -7,13 +7,39 @@
 # Assignment: Print Math 
 # Date:       8/27/2025
 
-WORDS = {}
+def remove_delimeters(string: str, delimeters: str) -> str:
+    string = list(string)
+    for delim in delimeters:
+        for idx, char in enumerate(string):
+            if char == delim:
+                del string[idx]
+    return "".join(string)
 
-def remove_delimeters() -> str:
-    
+def word_amounts(sentence: str) -> dict:
+    WORDS = {}
+    sentence = remove_delimeters(sentence, ".,?!").lower().split()
+    for word in sentence: 
+        WORDS[word] = 0
+    for word in sentence:
+        if WORDS.get(word) is not None:
+            WORDS[word] += 1
+    return WORDS
 
-def print_word_amounts(sentence: str, words: str):
-    s = sentence.strip(".,?!")
-    print(s)
+# sentence = "How much wood would a wood chuck chuck if a wood chuck could chuck wood?"
+# count_words = "wood chuck"
+sentence = input("Enter a sentence: ")
+count_words = input("Enter some words: ").lower().split()
+WORDS = word_amounts(sentence)
 
-print_word_amounts("Hullabaloo, Caneck! Caneck! Hullabaloo, Caneck! Caneck!", "Hullabaloo Caneck Whoop")
+for word in count_words:
+    count = WORDS.get(word)
+    status = f"appears {count} times"
+    if count is None:
+        status = "does not appear"
+
+    print(f"The word \"{word}\" {status} in the sentence")
+
+unique = 0
+for word in WORDS:
+    unique = unique + 1 if WORDS.get(word) > 0 else unique
+print(f"There are {unique} unique words in the sentence")
