5e0272bc43769ade9cb2a25e86b131becdfbddcd
EthanMik <etmik@icloud.com>
2025-10-20 13:46:22 -0500

    Unit 9 Work

---
+++ Unit_9/small_functions.py
@@ -0,0 +1,73 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   *I have not given or received any unauthorized aid on this assignment."
+#
+# Name:       Ethan Mikolaycik
+# Section:    563
+# Assignment: Print Math 
+# Date:       8/27/2025
+
+from math import *
+
+# part a
+def parta(nums: list[int]):
+    s = sorted(nums)
+    return [s[0], s[len(s) // 2], s[-1]]
+
+# part b
+def partb(time: list[float], dist: list[float]):
+    velocity = []
+    for i, val in enumerate(time[:-1]):
+        slope = (dist[i + 1] - dist[i]) / (time[i + 1] - time[i])
+        velocity.append(slope)
+    return velocity
+
+# part c
+def partc(nums: list[float]):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i == j: continue
+            if nums[i] + nums[j] == 2029: 
+                return nums[i] * nums[j]
+    return False
+
+# part d
+def partd(n: int):
+    pass 
+
+# part e
+def parte(r_sphere: float, r_hole: float):
+    return (4 * pi / 3) * (r_sphere**2 - r_hole**2)**1.5
+
+# part f
+def partf(char: str, name: str, company: str, email: str):
+    card = [name, company, email]
+    width = len(max(card))
+    if width % 2 == 0: width += 6
+    else: width += 4 
+    top = char * (width + 2)
+
+    card_full = []
+    card_full.append(top + '\n')
+    for i, val in enumerate(card):
+        line = f"{val:^{width}}"
+        card_full.append(char + line + char + "\n")
+    card_full.append(top)
+    
+    return "".join(card_full)
+
+# part g
+def partg(x: float, tolerance: float):
+    if x < -1 or x > 1: return None
+    sum = 0
+    n = 1
+    v = 1
+    while True:
+        v = (2 / (2 * n - 1)) * x**(2 * n - 1)
+        if abs(v) < tolerance: break
+        sum += v
+        n += 1
+
+    return sum
+
+
